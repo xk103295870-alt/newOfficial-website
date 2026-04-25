60993b06dd97236d2732647bfa7fd39f94c479ab
xiaoke321 <xk103295870@gmail.com>
2026-04-25 22:23:19 +0800

    feat: 新增 Canvas Workbench 产品页，更新导航、多语言翻译、Favicon

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -93,6 +93,7 @@ export default defineConfig({
   },
   
   head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     ['link', { href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap', rel: 'stylesheet' }]
